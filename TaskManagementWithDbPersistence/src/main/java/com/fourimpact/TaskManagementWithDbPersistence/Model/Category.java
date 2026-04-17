b0614556b22087a8d74c6efd2332547eda07b2de
Jordan Gonzales <jordanmenddozagonzales31@gmail.com>
2026-04-17 09:28:02 +0800

    Added Category Entity

---
+++ TaskManagementWithDbPersistence/src/main/java/com/fourimpact/TaskManagementWithDbPersistence/Model/Category.java
@@ -0,0 +1,33 @@
+package com.fourimpact.TaskManagementWithDbPersistence.Model;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    // Fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(name = "name", nullable = false, length = 150)
+    private String name;
+
+    @Column(name = "description",columnDefinition = "NVARCHAR(MAX")
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Task> tasks = new ArrayList<>();
+
+    // Constructors
+    public Category() {}
+
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}

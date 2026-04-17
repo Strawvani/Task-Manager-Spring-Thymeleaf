cad04a087358aec9c2c62115f8be3c1ff47533b8
Jordan Gonzales <jordanmenddozagonzales31@gmail.com>
2026-04-17 17:25:24 +0800

    Added UserRepository

---
+++ TaskManagementWithDbPersistence/src/main/java/com/fourimpact/TaskManagementWithDbPersistence/Repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.fourimpact.TaskManagementWithDbPersistence.Repository;
+
+import com.fourimpact.TaskManagementWithDbPersistence.Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
